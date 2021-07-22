adda0a56d3ffbeb5fa53682cdc4d62fdea58bc8f
zhangqianyu.sys <zhangqianyu.sys@bytedance.com>
2021-07-22 08:22:27 +0000

    update ExampleHandler

--- ExampleHandler.h
+++ ExampleHandler.h
@@ -3,8 +3,9 @@
 #include <glog/logging.h>
 #include "./gen-cpp2/ExampleService.h"
 
-namespace tamvm {
-namespace cpp2 {
+namespace tamvm { namespace cpp2 {
+
+
 class ExampleHandler: public ExampleServiceSvIf {
 public:
 	int64_t get_number(int32_t n) override {
@@ -12,5 +13,6 @@ public:
 		return n + 10;
 	}
 };
-}
-}
+
+
+}} // namespace tamvm::cpp2
